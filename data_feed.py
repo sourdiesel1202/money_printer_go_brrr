af26ed29994ecd85ea20b2f66dd980077c9a7af2
Andrew <andrew@Andrews-MBP.lan>
2023-07-07 09:47:00 -0400

    added static NYSE listing; starting building alert functions

--- data_feed.py
+++ data_feed.py
@@ -6,9 +6,9 @@
 import polygon, datetime
 import time
 from zoneinfo import ZoneInfo
-from indicators import load_macd, load_sma, load_dmi
+from indicators import load_macd, load_sma, load_dmi_adx, load_rsi, did_macd_alert, did_rsi_alert
 from history import load_ticker_history_raw, load_ticker_history_pd_frame
-from functions import  load_module_config
+from functions import  load_module_config, read_csv
 module_config = load_module_config(__file__.split("/")[-1].split(".py")[0])
 
 def print_hi(name):
@@ -19,32 +19,27 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     client = polygon.RESTClient(api_key=module_config['api_key'])
-    ticker = "GE"
-
-    macd_data =load_macd(ticker, client,timespan="hour", )
-    sma_data =load_sma(ticker, client,timespan="hour", )
-    # ticker_history = load_ticker_history(ticker, client,multiplier=1, timespan="hour", from_="2023-07-06", to="2023-07-06",limit=50)
-
-    aggs = []
-    dmi = load_dmi(ticker,client)
+    # ticker = "GE"
+    data_lines = read_csv("data/nyse.csv")
+    for i in range(1, len(data_lines)):
+        ticker = data_lines[i][0]
+        if '$' in ticker:
+            continue
+        try:
+            print(f"Checking ticker: {ticker}")
+            macd_data =load_macd(ticker, client,module_config,timespan="hour", )
+            if did_macd_alert(macd_data, ticker=ticker):
+                print("alert worked")
+            if did_rsi_alert(load_rsi(ticker, client,module_config,), ticker=ticker):
+                print("RSI Alerted")
+            # sma_data =load_sma(ticker, client,module_config,timespan="hour", )
+            # # ticker_history = load_ticker_history(ticker, client,module_config,multiplier=1, timespan="hour", from_="2023-07-06", to="2023-07-06",limit=50)
+            #
+            # aggs = []
+            # dmi = load_dmi_adx(ticker,client)
+            # rsi = load_rsi(ticker, client)
+        except:
+            print(f"Cannot process ticker: {ticker}")
     # ticker_history = load_ticker_history_raw(ticker,client,1, "hour", "2023-07-06","2023-07-06",5000)
     # ticke = load_ticker_history_pd_frame(ticker,client,1, "hour", "2023-07-06","2023-07-06",5000)
 
-    pass
-    # aggs = []
-    # for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="hour", from_="2023-07-06", to="2023-07-06",
-    #                           limit=50000):
-    #     aggs.append(a)
-    #
-    # print(aggs)
-    # List Aggregates (Bars)
-        # aggs = []
-
-        # for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="hour", from_="2023-07-06", to="2023-07-06",
-        #                           limit=50):
-        #     pass
-            # aggs.append(a)
-        #
-        # print(aggs)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- indicators.py
+++ indicators.py
@@ -3,44 +3,95 @@ import datetime
 from zoneinfo import ZoneInfo
 from history import load_ticker_history_pd_frame, load_ticker_history_csv
 from stockstats import wrap
-def load_macd(ticker, client, **kwargs):
+def load_macd(ticker, client,module_config,  **kwargs):
     macd = client.get_macd(ticker=ticker, **kwargs)
+    _macd = []
     for entry in macd.values:
-        if entry.value > entry.signal:
-            entry_date = datetime.datetime.fromtimestamp(entry.timestamp / 1e3, tz=ZoneInfo('US/Eastern'))
-            # entry_date.tzinfo = ZoneInfo('US/Eastern')
-            print(f"{entry_date}: {ticker}: MACD {entry.value} was over signal {entry.signal}: histogram {entry.histogram}")
-        else:
-            entry_date = datetime.datetime.fromtimestamp(entry.timestamp / 1e3, tz=ZoneInfo('US/Eastern'))
-            # entry_date.tzinfo = ZoneInfo('US/Eastern')
-            print(f"{entry_date}: {ticker}: Signal {entry.signal} was over MACD {entry.value}: histogram {entry.histogram}")
-    return macd
+        if module_config['logging']:
+            if entry.value > entry.signal:
+                entry_date = datetime.datetime.fromtimestamp(entry.timestamp / 1e3, tz=ZoneInfo('US/Eastern'))
+                # entry_date.tzinfo = ZoneInfo('US/Eastern')
+                print(f"{entry_date}: {ticker}: MACD {entry.value} was over signal {entry.signal}: histogram {entry.histogram}")
+            else:
+                entry_date = datetime.datetime.fromtimestamp(entry.timestamp / 1e3, tz=ZoneInfo('US/Eastern'))
+                # entry_date.tzinfo = ZoneInfo('US/Eastern')
+                print(f"{entry_date}: {ticker}: Signal {entry.signal} was over MACD {entry.value}: histogram {entry.histogram}")
+        _macd.append(entry)
+    return _macd
 
-def load_sma(ticker, client, **kwargs):
+def load_sma(ticker, client,module_config, **kwargs):
     sma = client.get_sma(ticker, **kwargs)
+    _sma = []
     for entry in sma.values:
-        entry_date = datetime.datetime.fromtimestamp(entry.timestamp / 1e3, tz=ZoneInfo('US/Eastern'))
-        # entry_date.tzinfo = ZoneInfo('US/Eastern')
-        print(f"{entry_date}: {ticker}: SMA {entry.value}")
+        if module_config['logging']:
+            entry_date = datetime.datetime.fromtimestamp(entry.timestamp / 1e3, tz=ZoneInfo('US/Eastern'))
+            # entry_date.tzinfo = ZoneInfo('US/Eastern')
+            print(f"{entry_date}: {ticker}: SMA {entry.value}")
+        _sma.append(entry)
+
 
     return sma
-def load_obv(ticker, client, **kwargs):
+def load_rsi(ticker, client,module_config, **kwargs):
+    rsi = client.get_rsi(ticker, timespan='hour')
+    _rsi = []
+    for entry in rsi.values:
+        if module_config['logging']:
+            entry_date = datetime.datetime.fromtimestamp(entry.timestamp / 1e3, tz=ZoneInfo('US/Eastern'))
+            # entry_date.tzinfo = ZoneInfo('US/Eastern')
+            print(f"{entry_date}: {ticker}: RSI {entry.value}")
+        _rsi.append(entry)
+    return _rsi
+def load_obv(ticker, client,module_config, **kwargs):
     pass
-def load_adx(ticker, client, **kwargs):
-    pass
-def load_dmi(ticker, client, **kwargs):
+# def load_adx(ticker, client, **kwargs):
+    # load_dmi(ticker,client,**kwargs)
+
+def load_dmi_adx(ticker, client,module_config, **kwargs):
+    '''
+    Returns a dict formatted like {'dmi+':<series_data>, 'dmi-':<series_data>, 'adx':<series_data>}
+    where keys in the series are timestamps as loaded in load_ticker_history
+    :param ticker:
+    :param client:
+    :param kwargs:
+    :return:
+    '''
     history_entries = load_ticker_history_csv(ticker,  client, 1, "hour", "2023-07-06", "2023-07-06", 500)
     print(f"{datetime.datetime.fromtimestamp(history_entries[1][0] / 1e3, tz=ZoneInfo('US/Eastern'))} {history_entries[1][0]}")
     print(f"{datetime.datetime.fromtimestamp(history_entries[1][-1] / 1e3, tz=ZoneInfo('US/Eastern'))} {history_entries[1][0]}")
     df= wrap(load_ticker_history_pd_frame(ticker, client, 1, "hour", "2023-07-06", "2023-07-06", 500))
-    dmi= [df['pdi'].get(history_entries[-1][0]), df['ndi'].get(history_entries[-1][0]), df['adx'].get(history_entries[-1][0])]
-    # for i in range(1, len(history_entries)):
-    #     print(f"{datetime.datetime.fromtimestamp(history_entries[i][0] / 1e3, tz=ZoneInfo('US/Eastern'))} DMI+: {dmi[0]} DMI-:{dmi[1]} ADX: {dmi[2]}")
+    dmi= {"dmi+":df['pdi'],"dmi-":df['ndi'], "adx":df['adx']}
+    for i in range(1, len(history_entries)):
+        print(f"{datetime.datetime.fromtimestamp(history_entries[i][0] / 1e3, tz=ZoneInfo('US/Eastern'))} DMI+: {dmi['dmi+'][history_entries[i][0]]} DMI-:{dmi['dmi-'][history_entries[i][0]]} ADX: {dmi['adx'][history_entries[i][0]]}")
     # # raw_data =
     return dmi
 
 
-def did_macd_alert(data):
+def did_macd_alert(data, ticker=""):
+    print(f"checking macd for {ticker}")
+    #ok so the idea here is to look at the data for n vs  n-1 where n is the most recent macd reading
+    if (data[0].value > data[0].signal and data[1].value < data[1].signal)  or (data[0].value < data[0].signal and data[1].value > data[1].signal):
+        entry_date = datetime.datetime.fromtimestamp(data[0].timestamp / 1e3, tz=ZoneInfo('US/Eastern'))
+        print(f"{entry_date}:{ticker}: MAC/Signal Crossover ")
+        return True
+    else:
+        return False
+
+
+
+    # ok so first things first,
+    pass
+def did_dmi_alert(data):
+    pass
+
+def did_rsi_alert(data, ticker=""):
+    if data[0].value > 80 or data[0].value < 50:
+        entry_date = datetime.datetime.fromtimestamp(data[0].timestamp / 1e3, tz=ZoneInfo('US/Eastern'))
+        print(f"{entry_date}:{ticker}: RSI Alerted at {data[0].value} ")
+        return True
+    else:
+        return  False
+def did_adx_alert(data):
+    pass
     '''
     Pass in the data from the client and do calculations
     :param data:
@@ -48,4 +99,9 @@ def did_macd_alert(data):
     '''
     pass
 def determine_macd_direction(data):
+    pass
+def determine_adx_direction(data):
+    pass
+
+def determine_dmi_direction(data):
     pass
