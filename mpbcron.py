77fc43e66311d533c71f40ae21b52d6be9a92d3e
Andrew <andrew@Andrews-MBP.lan>
2023-07-13 16:21:53 -0400

    trying to work in AWS

--- mpbcron.py
+++ mpbcron.py
@@ -5,4 +5,5 @@ while True:
     if datetime.datetime.now().minute ==18:
         os.system("source /var/virtual_environments/mpb/bin/activate && cd /var/app/money_printer_go_brrr && python market_scanner.py > /var/log/mpb.log")
     else:
+        print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%D:%S')}:It's not time to run yet, going to sleep")
         time.sleep(30)
