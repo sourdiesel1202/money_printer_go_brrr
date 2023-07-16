31b7da135a1f98c44a9713d9dedb6eedf521d452
Andrew <andrew@Andrews-MBP.lan>
2023-07-16 14:53:38 -0400

    Options entry/exit basic logic added

--- options.py
+++ options.py
@@ -24,17 +24,17 @@ def calculate_price_change_for_entry(price_goal_change_type, position_type,asset
             print(f"{price_goal} percent of ${asset_price}: ${p}")
             _asset_price = asset_price - p
         else:
-            _asset_price = asset_price
+            _asset_price = price_goal
         _tmp_price = asset_price
         _tmp_ask = ask
         _tmp_iv = mibian.BS([_tmp_price, strike_price, 0, dte], callPrice=_tmp_ask).impliedVolatility
         greeks = mibian.BS([_tmp_price, strike_price, 0, dte], volatility=_tmp_iv)
         per_cent_delta = float(greeks.callDelta if position_type == PositionType.LONG else greeks.putDelta )/ 100
-        while round(_tmp_price,2) != round(price_goal,2):# and _tmp_ask > 0.05: #stop at a 0.05 ask, etrade order price min increment lol
-            if round(_tmp_price, 2) == round(price_goal, 2):
+        while round(_tmp_price,2) != round(_asset_price,2):# and _tmp_ask > 0.05: #stop at a 0.05 ask, etrade order price min increment lol
+            if round(_tmp_price, 2) == round(_asset_price, 2):
                 break
-            _tmp_price = _tmp_price -  0.01 *( 1 if position_type == PositionType.LONG else -1) # one send decrements
-            _tmp_ask = _tmp_ask - per_cent_delta
+            _tmp_price = _tmp_price -  0.01 *(1 if position_type == PositionType.LONG else -1) # one send decrements
+            _tmp_ask = _tmp_ask - per_cent_delta * (1 if position_type == PositionType.LONG else -1)
             _tmp_iv = mibian.BS([_tmp_price, strike_price, 0, dte], callPrice=_tmp_ask).impliedVolatility
             greeks = mibian.BS([_tmp_price, strike_price, 0, dte], volatility=_tmp_iv)
             per_cent_delta = float(greeks.callDelta if position_type == PositionType.LONG else greeks.putDelta )/ 100
@@ -62,30 +62,25 @@ def calculate_price_change_for_entry(price_goal_change_type, position_type,asset
     #     raise Exception(f"Just slap the ask {ask}, it's less than your bid {bid}")
 
     # while _tmp_ask > bid and _tmp_ask > 0.05: #stop at a 0.05 ask, etrade order price min increment lol
-    while round(_tmp_ask,2) != round(bid,2) and _tmp_ask > 0.05: #stop at a 0.05 ask, etrade order price min increment lol
+    while round(_tmp_ask,2) >= round(bid,2) and _tmp_ask > 0.05: #stop at a 0.05 ask, etrade order price min increment lol
+        if round(_tmp_ask,2) <= round(bid,2):
+            break
         _tmp_price = _tmp_price -  0.01 *( 1 if position_type == PositionType.LONG else -1)
-        _tmp_ask = _tmp_ask - per_cent_delta
+        _tmp_ask = _tmp_ask - per_cent_delta *(1 if position_type == PositionType.LONG else -1)
         _tmp_iv = mibian.BS([_tmp_price, strike_price, 0, dte], callPrice=_tmp_ask).impliedVolatility
         greeks = mibian.BS([_tmp_price, strike_price, 0, dte], volatility=_tmp_iv)
         per_cent_delta = float(greeks.callDelta if position_type == PositionType.LONG else greeks.putDelta ) /100
 
 
-    # else:
-    #     while _tmp_ask > bid and _tmp_ask > 0.05:  # stop at a 0.05 ask, etrade order price min increment lol
-    #         _tmp_price = _tmp_price + 0.01  # one send decrements
-    #         _tmp_ask = _tmp_ask + per_cent_delta
-    #         # ok so calculate new ask and price first, we will need them to calculate the rest
-    #         #
-    #         _tmp_iv = mibian.BS([_tmp_price, strike_price, 0, dte], callPrice=_tmp_ask).impliedVolatility
-    #         greeks = mibian.BS([_tmp_price, strike_price, 0, dte], volatility=_tmp_iv)
-    #         per_cent_delta = float(greeks.putDelta / 100)
 
     if round(bid) >= round(_tmp_ask):
         #assuming we get here, we know that the order would have been filled
         return {
             "asset_price": round(_tmp_price,2),
-            "change": round(float(_tmp_price - asset_price),2),
-            "percentage_change": round(calculate_percentage(float(_tmp_price - asset_price), asset_price),2),
+            "asset_change": round(float(_tmp_price - asset_price),2),
+            "asset_percentage_change": round(calculate_percentage(float(_tmp_price - asset_price), asset_price),2),
+            "bid": bid,
+            "ask": ask,
             "greeks": {
                 "delta": greeks.callDelta if position_type == PositionType.LONG else greeks.putDelta,
                 "theta": greeks.callTheta if position_type == PositionType.LONG else greeks.putTheta,
@@ -99,47 +94,97 @@ def calculate_price_change_for_entry(price_goal_change_type, position_type,asset
         raise Exception(f"Decreased call price from {asset_price} ==> {_tmp_price} did not reach bid price")
 
 
-def calculate_price_change_for_exit(price_goal_change_type, goal, position_type,asset_price,strike_price, bid, ask,  dte):
+def calculate_price_change_for_exit(price_goal_change_type, position_type,asset_price,strike_price, price_goal, ask,  dte):
     ##in THEORY this should work for futures too?
     #basically, how much does the price need to change before our bid is met
     #can't think of a better way to do this than by the penny
     # _tmp_iv =
     _tmp_iv = mibian.BS([asset_price, strike_price, 0, dte], callPrice=ask).impliedVolatility
     greeks = mibian.BS([asset_price, strike_price, 0, dte], volatility=_tmp_iv)
+    per_cent_delta = float(greeks.callDelta / 100 if position_type == PositionType.LONG else greeks.putDelta / 100)
 
-    per_cent_delta = float(greeks.callDelta/100 if position_type == PositionType.LONG else greeks.putDelta / 100)
+    if price_goal_change_type   in [PriceGoalChangeType.ASSET_POINTS, PriceGoalChangeType.ASSET_PERCENTAGE, PriceGoalChangeType.ASSET_SET_PRICE]:
+        #ok so basically we need to figure out what the bid is at our target asking price
+        if price_goal_change_type == PriceGoalChangeType.ASSET_POINTS:
+            _asset_price = asset_price - price_goal
+        elif price_goal_change_type == PriceGoalChangeType.ASSET_PERCENTAGE:
+            p = float(float(price_goal / 100) * asset_price)
+            print(f"{price_goal} percent of ${asset_price}: ${p}")
+            _asset_price = asset_price - p
+        else:
+            _asset_price = price_goal
+        _tmp_price = asset_price
+        _tmp_ask = ask
+        _tmp_iv = mibian.BS([_tmp_price, strike_price, 0, dte], callPrice=_tmp_ask).impliedVolatility
+        greeks = mibian.BS([_tmp_price, strike_price, 0, dte], volatility=_tmp_iv)
+        per_cent_delta = float(greeks.callDelta if position_type == PositionType.LONG else greeks.putDelta )/ 100
+        while round(_asset_price,2) != round(_tmp_price,2):
+        # while (round(_tmp_price,2) <= round(_asset_price,2) if position_type == PositionType.LONG else _asset_price <= round(_tmp_price,2)) :# and _tmp_ask > 0.05: #stop at a 0.05 ask, etrade order price min increment lol
+            # if round(_tmp_price, 2) == round(_asset_price, 2):
+            #     break
+            _tmp_price = _tmp_price - 0.01 * (-1 if position_type == PositionType.LONG else 1)  # one send decrements
+            _tmp_ask = _tmp_ask - per_cent_delta * (-1 if position_type == PositionType.LONG else 1)
+            _tmp_iv = mibian.BS([_tmp_price, strike_price, 0, dte], callPrice=_tmp_ask).impliedVolatility
+            greeks = mibian.BS([_tmp_price, strike_price, 0, dte], volatility=_tmp_iv)
+            per_cent_delta = float(greeks.callDelta if position_type == PositionType.LONG else greeks.putDelta )/ 100
+        bid = _tmp_ask
+        # _tmp_price = _asset_price
+    else:
+
+        #basically for all cases but asset changes, we can more ore less re-use the existing code, we just need to set our bid accordingly
+        # if price_goal_change_type == PriceGoalChangeType.OPTION_SET_PRICE:
+        if price_goal_change_type == PriceGoalChangeType.OPTION_TICK:
+            bid = ask - price_goal
+        elif price_goal_change_type == PriceGoalChangeType.OPTION_PERCENTAGE:
+            p = float(float(price_goal/100)*ask)
+            print(f"{price_goal} percent of ${strike_price} {position_type} Option priced at ${ask}: ${p}")
+            bid  = ask -p
+        else:
+            bid = price_goal
+
+    # bid = bid
     # per_cent_gamma = float(gamma / 100)
     #don't care about vega rn
     _tmp_ask = ask
     _tmp_price = asset_price
-    if bid > ask:
-        raise Exception(f"Just slap the ask {ask}, it's less than your bid {bid}")
-    if position_type == PositionType.LONG: #positive delta first
+    # if bid > ask:
+    #     raise Exception(f"Just slap the ask {ask}, it's less than your bid {bid}")
 
-        while _tmp_ask > bid and _tmp_ask > 0.05: #stop at a 0.05 ask, etrade order price min increment lol
-            _tmp_price = _tmp_price - 0.01 #one send decrements
-            _tmp_ask = _tmp_ask - per_cent_delta
-            _tmp_iv = mibian.BS([_tmp_price, strike_price, 0, dte], callPrice=_tmp_ask).impliedVolatility
-            greeks = mibian.BS([_tmp_price, strike_price, 0, dte], volatility=_tmp_iv)
-            per_cent_delta = float(greeks.callDelta/100)
+    # while _tmp_ask < bid: #stop at a 0.05 ask, etrade order price min increment lol
+    # while (round(_tmp_ask,2) < round(bid,2) if position_type == PositionType.LONG else round(bid,2) < round(_tmp_ask,2)):
+    while _tmp_ask <= bid:# and _tmp_ask > 0.05: #stop at a 0.05 ask, etrade order price min increment lol
 
 
-    else:
-        while _tmp_ask > bid and _tmp_ask > 0.05:  # stop at a 0.05 ask, etrade order price min increment lol
-            _tmp_price = _tmp_price + 0.01  # one send decrements
-            _tmp_ask = _tmp_ask + per_cent_delta
-            # ok so calculate new ask and price first, we will need them to calculate the rest
-            #
-            _tmp_iv = mibian.BS([_tmp_price, strike_price, 0, dte], callPrice=_tmp_ask).impliedVolatility
-            greeks = mibian.BS([_tmp_price, strike_price, 0, dte], volatility=_tmp_iv)
-            per_cent_delta = float(greeks.putDelta / 100)
+        if _tmp_ask >= bid:
+            break
+        _tmp_price = _tmp_price - 0.01 * (-1 if position_type == PositionType.LONG else 1)
+        _tmp_ask = _tmp_ask - per_cent_delta * (-1 if position_type == PositionType.LONG else 1)
+        _tmp_iv = mibian.BS([_tmp_price, strike_price, 0, dte], callPrice=_tmp_ask).impliedVolatility
+        greeks = mibian.BS([_tmp_price, strike_price, 0, dte], volatility=_tmp_iv)
+        per_cent_delta = float(greeks.callDelta if position_type == PositionType.LONG else greeks.putDelta ) /100
 
-    if bid >= _tmp_ask:
+        # if round(_tmp_ask, 2) == round(bid, 2):
+        #     break
+
+
+    # else:
+    #     while _tmp_ask > bid and _tmp_ask > 0.05:  # stop at a 0.05 ask, etrade order price min increment lol
+    #         _tmp_price = _tmp_price + 0.01  # one send decrements
+    #         _tmp_ask = _tmp_ask + per_cent_delta
+    #         # ok so calculate new ask and price first, we will need them to calculate the rest
+    #         #
+    #         _tmp_iv = mibian.BS([_tmp_price, strike_price, 0, dte], callPrice=_tmp_ask).impliedVolatility
+    #         greeks = mibian.BS([_tmp_price, strike_price, 0, dte], volatility=_tmp_iv)
+    #         per_cent_delta = float(greeks.putDelta / 100)
+
+    if round(bid,2) <= round(_tmp_ask,2):
         #assuming we get here, we know that the order would have been filled
         return {
             "asset_price": round(_tmp_price,2),
-            "change": round(float(_tmp_price - asset_price),2),
-            "percentage_change": round(calculate_percentage(float(_tmp_price - asset_price), asset_price),2),
+            "asset_change": round(float(_tmp_price - asset_price),2),
+            "asset_percentage_change": round(calculate_percentage(float(_tmp_price - asset_price), asset_price),2),
+            "bid": round(ask,2),
+            "ask": round(bid,2),
             "greeks": {
                 "delta": greeks.callDelta if position_type == PositionType.LONG else greeks.putDelta,
                 "theta": greeks.callTheta if position_type == PositionType.LONG else greeks.putTheta,
--- options_tester.py
+++ options_tester.py
@@ -7,7 +7,28 @@ from history import load_ticker_history_raw
 from validation import validate_ticker
 from functions import load_module_config, get_today
 module_config = load_module_config(__file__.split("/")[-1].split(".py")[0])
-from options import calculate_price_change_for_entry,calculate_dte
+from options import calculate_price_change_for_entry,calculate_dte, calculate_price_change_for_exit
+'''
+
+Use this to create options_tester.json in configs/
+
+
+
+{
+  "asset_price": 14.98,
+  "position_type": "LONG",
+  "ask": 0.39,
+  "strike_price": 15,
+  "ticker": "F",
+  "expiration_date": "2023-07-28",
+  "bid_price_goal_type": "ASSET_SET_PRICE",
+  "ask_price_gol_type": "ASSET_SET_PRICE",
+  "bid_price_goal": 14.98,
+  "ask_price_goal": 14.98
+
+}
+
+'''
 if __name__ == '__main__':
     start_time = time.time()
 
@@ -16,27 +37,43 @@ if __name__ == '__main__':
     # def calculate_price_change_for_entry(asset_price, position_type,strike_price, bid_price_goal, ask,  dte):
     # print('\n'.join([f"{k}: {v}" for k,v in module_config.items()]))
 
-
-    module_config['bid_price_goal'] = module_config['asset_price']-0.10
+    print(f"#### TESTING ENTRY POSITIONS ####")
+    module_config['bid_price_goal'] = module_config['asset_price'] - 0.10
     print(f"\nIn order to take a {module_config['position_type']} position ({PositionType.LONG_OPTION if module_config['position_type'] == PositionType.LONG else PositionType.SHORT_OPTION}) in $({module_config['ticker']}) at bid price ${module_config['bid_price_goal']} (current ask: {module_config['ask']}), the following price change needs to occur")
     # def calculate_price_change_for_entry(price_goal_change_type, position_type, asset_price, strike_price, price_goal,ask, dte):
-    result = calculate_price_change_for_entry(module_config['bid_price_goal_type'],module_config['position_type'],module_config['asset_price'], module_config['strike_price'],module_config['bid_price_goal'], module_config['ask'], calculate_dte(module_config['expiration_date']) )
-    print('\n'.join([f"{k}: {v}" for k,v in result.items()]))
+    result = calculate_price_change_for_entry(module_config['bid_price_goal_type'], module_config['position_type'],
+                                             module_config['asset_price'], module_config['strike_price'],
+                                             module_config['bid_price_goal'], module_config['ask'],
+                                             calculate_dte(module_config['expiration_date']))
+    print('\n'.join([f"{k}: {v}" for k, v in result.items()]))
 
     module_config['bid_price_goal'] = module_config['asset_price'] + 0.10
-    print(f"\nIn order to take a {PositionType.SHORT} position ({PositionType.SHORT_OPTION }) in $({module_config['ticker']}) at bid price ${module_config['bid_price_goal']} (current ask: {module_config['asset_price']}), the following price change needs to occur")
-    result = calculate_price_change_for_entry(module_config['bid_price_goal_type'], PositionType.SHORT, module_config['asset_price'],
-                                              module_config['strike_price'], module_config['bid_price_goal'], module_config['ask'],
+    print(
+        f"\nIn order to take a {PositionType.SHORT} position ({PositionType.SHORT_OPTION}) in $({module_config['ticker']}) at bid price ${module_config['bid_price_goal']} (current ask: {module_config['asset_price']}), the following price change needs to occur")
+    result = calculate_price_change_for_entry(module_config['bid_price_goal_type'], PositionType.SHORT,
+                                             module_config['asset_price'],
+                                             module_config['strike_price'], module_config['bid_price_goal'],
+                                             module_config['ask'],
+                                             calculate_dte(module_config['expiration_date']))
+    print('\n'.join([f"{k}: {v}" for k, v in result.items()]))
+
+
+
+    print(f"#### TESTING EXIT POSITIONS ####")
+    module_config['ask_price_goal'] = module_config['asset_price']+0.20
+    print(f"\nIn order to exit  a {module_config['position_type']} position ({PositionType.LONG_OPTION if module_config['position_type'] == PositionType.LONG else PositionType.SHORT_OPTION}) in $({module_config['ticker']}) at bid price ${module_config['ask_price_goal']} (current ask: {module_config['ask']}), the following price change needs to occur")
+    # def calculate_price_change_for_entry(price_goal_change_type, position_type, asset_price, strike_price, price_goal,ask, dte):
+    result = calculate_price_change_for_exit(module_config['bid_price_goal_type'],module_config['position_type'],module_config['asset_price'], module_config['strike_price'],module_config['ask_price_goal'], module_config['ask'], calculate_dte(module_config['expiration_date']) )
+    print('\n'.join([f"{k}: {v}" for k,v in result.items()]))
+
+    module_config['ask_price_goal'] = module_config['asset_price'] - 0.20
+    print(f"\nIn order to exit a {PositionType.SHORT} position ({PositionType.SHORT_OPTION }) in $({module_config['ticker']}) at goal {module_config['ask_price_goal']} {module_config['ask_price_goal']}  (current ask: {module_config['asset_price']}), the following price change needs to occur")
+    result = calculate_price_change_for_exit(module_config['bid_price_goal_type'], PositionType.SHORT, module_config['asset_price'],
+                                              module_config['strike_price'], module_config['ask_price_goal'], module_config['ask'],
                                               calculate_dte(module_config['expiration_date']))
     print('\n'.join([f"{k}: {v}" for k, v in result.items()]))
-    pass
-    # client = polygon.RESTClient(api_key=module_config['api_key'])
-    # # ticker = "GE"
-    # for ticker in module_config['tickers']:
-    #     ticker_history = load_ticker_history_raw(ticker, client, 1, module_config['timespan'],get_today(module_config, minus_days=7), get_today(module_config), 50000, module_config)
-    #     if module_config['run_concurrently']:
-    #         validate_ticker(module_config['position_type'],ticker,ticker_history, module_config)
-    #     else:
-    #         validate_ticker(module_config['position_type'],ticker,ticker_history, module_config)
+    # pass
+
+
 
     print(f"\nCompleted testing  of ({module_config['ticker']}) {PositionType.LONG_OPTION if module_config['position_type'] == PositionType.LONG else PositionType.SHORT_OPTION} in {int((int(time.time()) - start_time) / 60)} minutes and {int((int(time.time()) - start_time) % 60)} seconds")
