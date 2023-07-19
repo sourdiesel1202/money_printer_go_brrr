fe871a6d3a0b80da9be12281df09612e059ca6f6
Andrew <andrew@Andrews-MBP.lan>
2023-07-19 14:01:24 -0400

    checking in cahnges

--- history.py
+++ history.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from enums import OrderType
 import datetime,io
@@ -15,6 +16,7 @@ class TickerHistory:
     low = 0
     volume = 0
     timestamp = 0
+    dt = None
     def __init__(self, open, close, high, low, volume, timestamp):
         self.open = open
         self.close = close
@@ -22,7 +24,7 @@ class TickerHistory:
         self.low = low
         self.volume = volume
         self.timestamp =timestamp
-
+        self.dt = timestamp_to_datetime(timestamp)
 def convert_ticker_history_to_csv(ticker, ticker_history):
     rows = [['o','c','h','l','v','t']]
     for history in ticker_history:
@@ -53,19 +55,30 @@ def clear_ticker_history_cache_entry(ticker, module_config):
 def load_ticker_history_raw(ticker,client, multiplier = 1, timespan = "hour", from_ = "2023-07-06", to = "2023-07-06", limit=500, module_config={}, cached=False):
     # ticker = ticker, multiplier = 1, timespan = "hour", from_ = today, to = today,
     # limit = 50000
+    if timespan == 'hour':
+        timespan = 'minute'
+        multiplier = 30
+        module_config['timespan_multiplier'] = multiplier
     if cached:
         return load_ticker_history_cached(ticker, module_config)
     else:
         if os.path.exists(f"{module_config['output_dir']}cached/{ticker}.csv"):
-            clear_ticker_history_cache_entry(module_config)
+            clear_ticker_history_cache_entry(ticker,module_config)
         history_data =  []
         for entry in client.list_aggs(ticker=ticker,multiplier = multiplier, timespan = timespan, from_ = from_, to = to, limit=50000, sort='asc'):
             entry_date = datetime.datetime.fromtimestamp(entry.timestamp / 1e3, tz=ZoneInfo('US/Eastern'))
             # print(f"{entry_date}: {ticker}| Open: {entry.open} High: {entry.high} Low: {entry.low} Close: {entry.close} Volume: {entry.volume}")
-            if datetime.datetime.fromtimestamp(entry.timestamp / 1e3,
-                                               tz=ZoneInfo('US/Eastern')).hour >= 10 and datetime.datetime.fromtimestamp(
-                    entry.timestamp / 1e3, tz=ZoneInfo('US/Eastern')).hour <= 15:
-                history_data.append(TickerHistory(entry.open, entry.close,entry.high, entry.low, entry.volume, entry.timestamp))
+            if (datetime.datetime.fromtimestamp(entry.timestamp / 1e3,tz=ZoneInfo('US/Eastern')).hour >= 9 if timespan =='minute' else 10) and (datetime.datetime.fromtimestamp(entry.timestamp / 1e3, tz=ZoneInfo('US/Eastern')).hour <= 16 if timespan =='minute' else 15):
+                if timespan == 'minute':
+                    if (datetime.datetime.fromtimestamp(entry.timestamp / 1e3,tz=ZoneInfo('US/Eastern'))).hour == 9 and (datetime.datetime.fromtimestamp(entry.timestamp / 1e3,tz=ZoneInfo('US/Eastern'))).minute < 30:
+                        continue
+                    elif (datetime.datetime.fromtimestamp(entry.timestamp / 1e3, tz=ZoneInfo('US/Eastern'))).hour >=16:
+                        continue
+                    else:
+                        history_data.append(TickerHistory(entry.open, entry.close, entry.high, entry.low, entry.volume,entry.timestamp))
+
+                else:
+                    history_data.append(TickerHistory(entry.open, entry.close,entry.high, entry.low, entry.volume, entry.timestamp))
 
         if module_config['test_mode']:
             if module_config['test_use_test_time']:
@@ -75,7 +88,9 @@ def load_ticker_history_raw(ticker,client, multiplier = 1, timespan = "hour", fr
                     if timestamp_to_datetime(history_data[-i].timestamp).hour == module_config['test_time']:
                         history_data = history_data[:-i+1]
                         break
-
+        if module_config['timespan'] == 'hour':
+            history_data = normalize_history_data_for_hour(ticker, history_data, module_config)
+        # if module_config['logging']:
         print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}:${ticker}: Latest History Record: {datetime.datetime.fromtimestamp(history_data[-1].timestamp / 1e3, tz=ZoneInfo('US/Eastern'))}:Oldest History Record: {datetime.datetime.fromtimestamp(history_data[0].timestamp / 1e3, tz=ZoneInfo('US/Eastern'))}:Total: {len(history_data)}")
         write_ticker_history_cached(ticker, history_data, module_config)
 
@@ -100,3 +115,82 @@ def load_ticker_history_pd_frame(ticker, ticker_history, convert_to_datetime=Fal
     return df
 
 # def load_ticker_
+
+def normalize_history_data_for_hour(ticker, ticker_history, module_config):
+    i = 1
+    complete = False
+
+    new_ticker_history = []
+    # reversed(ticker_history)
+    # start = timestamp_to_datetime(ticker_history[-1].timestamp)
+    _th = []
+    #ok so first let's go ahead and correct all the ticker histories
+    # for i in range(1, len(ticker_history)):
+    #     adjusted_date = timestamp_to_datetime(ticker_history[-i].timestamp) -datetime.timedelta(minutes=module_config['timespan_multiplier'])
+    #     adjusted_timestsamp  = int(float(adjusted_date.strftime('%s.%f')) * 1e3)
+    #     _th.append(TickerHistory(ticker_history[-i].open, ticker_history[-i].close, ticker_history[-i].high,ticker_history[-i].low, ticker_history[-i].volume, adjusted_timestsamp))
+    # for i in range(1, len(_th)+1):
+    #     new_ticker_history.append(_th[-i])
+    # reversed(_th)
+    # last_bar_of_day = 30 if module_config['timespan'] == 'minute' or module_config['timespan'] != 'minue'
+    # reversed(_th)
+    # dumber = [timestamp_to_datetime(x.timestamp) for x in new_ticker_history]
+    # ticker_history = new_ticker_history
+    # dumb = [timestamp_to_datetime(x.timestamp) for x in ticker_history]
+    i = 1
+    # for i in range(1, ):
+    result = []
+    while True:
+        if i > (len(ticker_history) - int(60 / module_config['timespan_multiplier'])):
+            break
+        normalized_time = timestamp_to_datetime(ticker_history[-i].timestamp)
+        if module_config['logging']:
+            print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}:${ticker}: Normalizing History Record: {datetime.datetime.fromtimestamp(ticker_history[-i].timestamp / 1e3, tz=ZoneInfo('US/Eastern'))}:")
+        # if ((normalized_time + datetime.timedelta(minutes=module_config['timespan_multiplier'])).hour == 10  and  (normalized_time + datetime.timedelta(minutes=module_config['timespan_multiplier'])).minute < module_config['timespan_multiplier']) or (normalized_time + datetime.timedelta(minutes=module_config['timespan_multiplier'])).hour < 10:
+        #     if module_config['logging']:
+        #         print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}:${ticker}: History Record: {datetime.datetime.fromtimestamp(ticker_history[-i].timestamp / 1e3, tz=ZoneInfo('US/Eastern'))}: Is PreMarket data, Skipping")
+        #     i = i+1
+        #     continue
+        if (normalized_time + datetime.timedelta(minutes=module_config['timespan_multiplier'])).hour == 16 and (normalized_time + datetime.timedelta(minutes=module_config['timespan_multiplier'])).minute == 0:
+            result.append(ticker_history[-i])
+            i = i + 1
+            continue
+        hour = {'opens':[ticker_history[-(i)].open],'closes':[ticker_history[-i].close],'highs':[ticker_history[-i].high], 'lows':[ticker_history[-i].low], 'volumes':[ticker_history[-i].volume], 'timestamps': [ticker_history[-i].timestamp]}
+        increment_idx = 0
+        for ii in range(1,int(60/module_config['timespan_multiplier'])):
+            if module_config['logging']:
+                print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}:${ticker}: Processing Previous Bar For: {datetime.datetime.fromtimestamp(ticker_history[-i].timestamp / 1e3, tz=ZoneInfo('US/Eastern'))}: {datetime.datetime.fromtimestamp(ticker_history[-(i+ii)].timestamp / 1e3, tz=ZoneInfo('US/Eastern'))}:")
+            hour['highs'].append(ticker_history[-(i+ii)].high)
+            hour['lows'].append(ticker_history[-(i+ii)].low)
+            hour['volumes'].append(ticker_history[-(i+ii)].volume)
+            hour['timestamps'].append(ticker_history[-(i+ii)].timestamp)
+            hour['opens'].append(ticker_history[-(i+ii)].open)
+            hour['closes'].append(ticker_history[-(i+ii)].close)
+            increment_idx = ii+1
+            # i = i +1
+        # if (timestamp_to_datetime(ticker_history[-i]) - timestamp_to_datetime(ticker_history[-(i+ii)])):
+        #     pass
+        now = datetime.datetime.now()
+        # if normalized_time.day == now.day and normalized_time.month == now.month and normalized_time.year == now.year:#and now.minute > 30:
+        if normalized_time.minute > 30:
+            th = TickerHistory(hour['opens'][-1], hour['closes'][0], max(hour['highs']), min(hour['lows']), sum(hour['volumes']), hour['timestamps'][-1])
+        else:
+            th = TickerHistory(hour['opens'][-1], hour['closes'][0], max(hour['highs']), min(hour['lows']),
+                               sum(hour['volumes']), hour['timestamps'][-1])
+        # else:
+        #     th = TickerHistory(hour['opens'][-1], hour['closes'][0], max(hour['highs']), min(hour['lows']), sum(hour['volumes']), hour['timestamps'][-1])
+        if module_config['logging']:
+            print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}:${ticker}: Normalized History Record: {datetime.datetime.fromtimestamp(th.timestamp / 1e3, tz=ZoneInfo('US/Eastern'))}:")
+        result.append(th)
+        i = i + (increment_idx)
+        # if timestamp_to_datetime(ticker_history[-i].timestamp).minute == 0 or timestamp_to_datetime(ticker_history[-i].timestamp).minute % module_config['timespan_multiplier'] == 0:
+        #     new_ticker_history.append(ticker_history[-i])
+        # else:
+    # sorted(result, key=lambda x: x.timestamp)
+    result.reverse()
+    return  result
+    # whilenot complete:
+    #     if i == len(ticker_history):
+    #         break
+
+    pass
--- history_tester.py
+++ history_tester.py
@@ -1,35 +1,35 @@
 import time
+from zoneinfo import ZoneInfo
+
 # from enums import AlertType, PositionType
 # from backtest import backtest_ticker, load_backtest_ticker_data, backtest_ticker_concurrently, load_backtest_results, analyze_backtest_results
 import polygon, datetime
 
 from history import load_ticker_history_raw
 # from validation import validate_ticker
-from functions import load_module_config, get_today, human_readable_datetime, timestamp_to_datetime
-
+from functions import load_module_config, get_today
 # module_config = load_module_config(__file__.split("/")[-1].split(".py")[0])
 module_config = load_module_config('market_scanner_tester')
-from support_resistance import find_support_resistance_levels
-# from shape import compare_tickers
-from history import  load_ticker_history_cached
-from indicators import load_support_resistance, is_trading_in_sr_band, determine_sr_direction
+from shape import compare_tickers
 
 if __name__ == '__main__':
     start_time = time.time()
     client = polygon.RESTClient(api_key=module_config['api_key'])
     # ticker = "GE"
-    # ticker_b = module_config['compare_ticker']
-    for ticker in module_config['tickers']:
-        # module_config['logging'] = True
-        _th = load_ticker_history_cached(ticker, module_config)[-180:]
-        for i in reversed(range(10, len(_th)-10)):
-            ticker_history = _th[:i]
-            if module_config['logging']:
-                print(f"{human_readable_datetime(timestamp_to_datetime(ticker_history[-1].timestamp))}:${ticker}: Testing Support Resistance (Last Close ${ticker_history[-1].close})")
-            if not is_trading_in_sr_band(load_support_resistance(ticker, ticker_history, module_config), ticker, ticker_history, module_config):
-                output = determine_sr_direction(load_support_resistance(ticker, ticker_history, module_config), ticker, ticker_history, module_config)
+    module_config['logging']=True
+    for ticker_a in module_config['tickers']:
+        # for ticker_b in module_config['compare_tickers']:
+        ticker_history_a = load_ticker_history_raw(ticker_a, client, module_config['timespan_multiplier'], module_config['timespan'],get_today(module_config, minus_days=6), get_today(module_config), 50000,module_config)
+        for i in range(0, len(ticker_history_a)):
+            print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}:${module_config['tickers'][0]}: Previous History Record: {datetime.datetime.fromtimestamp(ticker_history_a[i].timestamp / 1e3, tz=ZoneInfo('US/Eastern'))}: Current History Record: {datetime.datetime.fromtimestamp(ticker_history_a[(i)].timestamp / 1e3, tz=ZoneInfo('US/Eastern'))}:|O:{ticker_history_a[(i)].open}|H:{ticker_history_a[(i)].high}|L:{ticker_history_a[(i)].low}|C:{ticker_history_a[(i)].close}|: Volume: {ticker_history_a[(i)].volume}")
 
-                print(f"{human_readable_datetime(timestamp_to_datetime(ticker_history[-1].timestamp))}:${ticker}: Testing Support Resistance (Last Close ${ticker_history[-1].close}) Alert Fired: {output}")
-
+        #     ticker_history_b = load_ticker_history_raw(ticker_b, client, 1, module_config['timespan'],get_today(module_config, minus_days=7), get_today(module_config), 50000, module_config)
+        #     compare_tickers(ticker_a, ticker_history_a, ticker_b, ticker_history_b, module_config)
+        # if module_config['run_concurrently']:
+            # determine_line_similarity(1,2)
+            # de(module_config['position_type'],ticker,ticker_history, module_config)
+        # else:
+            # determine_line_similarity(1, 2)
+            # validate_ticker(module_config['position_type'],ticker,ticker_history, module_config)
 
     print(f"\nCompleted {module_config['position_type']} validation of ({','.join([f'${x}' for x in module_config['tickers']])}) in {int((int(time.time()) - start_time) / 60)} minutes and {int((int(time.time()) - start_time) % 60)} seconds")
