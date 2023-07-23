ec676efa02ba2e131fe81101e4617fbe3ae03202
Andrew <andrew@Andrews-MBP.lan>
2023-07-22 20:50:20 -0400

    fix for aws

--- mpbcron.py
+++ mpbcron.py
@@ -3,7 +3,7 @@ import time
 
 while True:
     if datetime.datetime.now().minute in [18,48]:
-        os.system(" python ./market_scanner.py")
+        os.system("/var/virtual_environments/mpb/bin/python3 ./market_scanner.py")
     else:
         print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%D:%S')}:It's not time to run yet, going to sleep")
         time.sleep(30)
